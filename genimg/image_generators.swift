f4f25b8f6393c76ba3698686f4c89c0c5d38d32c
Michael Teter <michael@michaelteter.com>
2025-04-02 02:01:34 -0500

    Auto-commit before run [Wed Apr  2 02:01:34 CDT 2025]

--- genimg/image_generators.swift
+++ genimg/image_generators.swift
@@ -105,6 +105,9 @@ func impCirDemo(_ gc: CGContext) {
     let t = CGFloat(i) / CGFloat(max(1, steps - 1)) // Normalize i to 0..1
     let currentPathRadius = radius
     
+    startAngle += CGFloat(i * 10)
+    if (startAngle > 360.0) { startAngle = startAngle - 360.0 }
+    
     // *** Calculate the scaled MAX radius for the point-circles on THIS path ***
     let currentMaxPointCircleRadius = calculateScaledValue(
       currentValue: currentPathRadius,
