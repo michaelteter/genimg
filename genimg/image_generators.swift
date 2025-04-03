4863e4c541bdedce02e8e590bee61c3a1ae33457
Michael Teter <michael@michaelteter.com>
2025-04-02 22:30:55 -0500

    Auto-commit before run [Wed Apr  2 22:30:55 CDT 2025]

--- genimg/image_generators.swift
+++ genimg/image_generators.swift
@@ -110,7 +110,7 @@ func impCirInner(
     }
     
     if chance(20) {
-      c = adjustLightness(of: c, by: CGFloat.random(in: -0.5 ... 0.2)) ?? c
+      c = adjustLightness(of: c, by: CGFloat.random(in: -0.5 ... 0.5)) ?? c
     }
     
     if chance(10) {
