aa8119ba801da7c1801ec9188c27f5d05c984b86
Michael Teter <michael@michaelteter.com>
2025-04-05 01:10:02 -0500

    Auto-commit before run [Sat Apr  5 01:10:02 CDT 2025]

--- genimg/image_generators.swift
+++ genimg/image_generators.swift
@@ -443,10 +443,10 @@ func impCirDemo(_ gc: CGContext) {
       radius: radius,
       startAngleDegrees: startAngle,
       arcDegrees: arcDegrees,
-      pointRectMaxHeight: randCFloat(in: 90...90), // Max length of the "train car" radially
+      pointRectMaxHeight: randCFloat(in: 50...50), // Max length of the "train car" radially
       pointRectWidthRange: 50.0 ... 50.0, //50.0 ... 90.0, // Width of the "train car" tangentially
       gapFactor: 1.6, // Determines spacing based on max width
-      taperScale: 0.5
+      taperScale: 0.4
     )
     
     
